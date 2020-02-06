3337c1ee7772d560dd32a0f4e9e61a00941828c7
Matthew Stewart <meticulous.planning@gmail.com>
2020-02-05 16:48:27 -0800

    adding javascript!

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+alert("Perpare for amazing transportation options!");
+
+confirm("If you're ready click to answer an important question.")
+
+var greeting = prompt("Like Boats? 1 = Yes 2 = No");
+var reply;
+
+if (greeting = 2) {
+    reply = 'You should check out the trucks and planes parts of our page!.';
+}
+else {
+    reply = 'Welcome to Our Boat Page!';
+}
+
+document.write('<h3>' + reply + '</h3>');
