93e0dbcd9c9fef713c42b2faa4434d3a3632264c
Matthew Stewart <meticulous.planning@gmail.com>
2020-02-06 18:13:37 -0800

    perfecting javascript functions and loops

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 function confirmClick() {
 var answer = confirm("If you're ready click to answer an important question.");
     console.log(answer);
-    if (answer) true {
+    if (answer){
     alert("Perpare for amazing transportation options!");
     }
     else {
@@ -13,11 +13,28 @@ function likeBoats() {
 var greeting = prompt("Like Boats? 1 = Yes 2 = No");
 var reply;
 
+while (greeting !== '1' && greeting !== '2'){
+    greeting = prompt("Like Boats? 1 = Yes 2 = No, and not any other values.");
+}
+
 if (greeting === "2") {
     reply = 'You should check out the trucks and planes parts of our page!.';
 }
 else {
     reply = 'Welcome to Our Boat Page!';
 }
+
 return reply;
+};
+
+function colorPick() {
+    var colorList = ["No Boat",'Red Boat', 'Yellow Boat', 'Blue Boat'];
+
+    for(var i = 1; i < colorList.length ; i++ ){
+    var colorAnswer = confirm("Want this many boats? " +i); {
+    if (colorAnswer){
+        alert("Nice!");
+        };
+}
+}
 }
