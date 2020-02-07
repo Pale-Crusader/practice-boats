4766ffd9ef5e1ef5f08aa6616e546f641c95b92d
Matthew Stewart <meticulous.planning@gmail.com>
2020-02-07 13:27:14 -0800

    testing the color boat loop I made

--- javascript/index.js
+++ javascript/index.js
@@ -1,40 +1,55 @@
 function confirmClick() {
-var answer = confirm("If you're ready click to answer an important question.");
-    console.log(answer);
+    var answer = confirm("If you're ready click to answer an important question.");
+
     if (answer){
-    alert("Perpare for amazing transportation options!");
+        alert("Perpare for amazing transportation options!");
     }
     else {
-    alert("Are you sure?");
-}
+        alert("Are you sure?");
+    }
 };
 
 function likeBoats() {
-var greeting = prompt("Like Boats? 1 = Yes 2 = No");
-var reply;
+    var greeting = prompt("Like Boats? 1 = Yes 2 = No");
+    var reply;
 
-while (greeting !== '1' && greeting !== '2'){
+    while (greeting !== '1' && greeting !== '2'){
     greeting = prompt("Like Boats? 1 = Yes 2 = No, and not any other values.");
-}
+    }
 
-if (greeting === "2") {
-    reply = 'You should check out the trucks and planes parts of our page!.';
-}
-else {
-    reply = 'Welcome to Our Boat Page!';
-}
+    if (greeting === "2") {
+        reply = 'You should check out the trucks and planes parts of our page!.';
+    }else {
+        reply = 'Welcome to Our Boat Page!';
+    }
 
-return reply;
+    return reply;
 };
 
 function colorPick() {
-    var colorList = ["No Boat",'Red Boat', 'Yellow Boat', 'Blue Boat'];
+    var colorList = ['Red Boat', 'Yellow Boat', 'Blue Boat'];
+    var redBoat = " a Red boat?";
+	var blueBoat = " a Blue boat?";
+	var yellowBoat = " a Yellow boat?";
+    var boatQuestion;
+    var threeStrikes;
 
-    for(var i = 1; i < colorList.length ; i++ ){
-    var colorAnswer = confirm("Want this many boats? " +i); {
-    if (colorAnswer){
-        alert("Nice!");
-        };
+    for(var i = 0; i < colorList.length ; i++ ){
+    if (i === 0){
+        boatQuestion = redBoat;
+    } else if (i === 1) {
+        boatQuestion = yellowBoat;
+    } else if (i === 2) {
+        boatQuestion = blueBoat;
+    }
+
+    var colorAnswer = confirm("Want " + boatQuestion); {
+    if (colorAnswer) {
+        alert("We have that!"); 
+        }
+    else {
+        alert("Sorry to hear that.")
+    }
 }
 }
 }
