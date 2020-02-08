9c6ad5ccbb5976d19394fe5f810e74eab6778a85
Matthew Stewart <meticulous.planning@gmail.com>
2020-02-07 16:58:55 -0800

    made a useful for loop

--- javascript/index.js
+++ javascript/index.js
@@ -26,30 +26,62 @@ function likeBoats() {
     return reply;
 };
 
-function colorPick() {
-    var colorList = ['Red Boat', 'Yellow Boat', 'Blue Boat'];
-    var redBoat = " a Red boat?";
-	var blueBoat = " a Blue boat?";
-	var yellowBoat = " a Yellow boat?";
-    var boatQuestion;
-    var threeStrikes;
+// function colorPick() {
 
-    for(var i = 0; i < colorList.length ; i++ ){
-    if (i === 0){
-        boatQuestion = redBoat;
-    } else if (i === 1) {
-        boatQuestion = yellowBoat;
-    } else if (i === 2) {
-        boatQuestion = blueBoat;
-    }
+//     var redBoat = " a Red boat?";
+// 	var blueBoat = " a Blue boat?";
+// 	var yellowBoat = " a Yellow boat?";
+//     var boatQuestion;
+//     var threeStrikes = 3;
 
-    var colorAnswer = confirm("Want " + boatQuestion); {
-    if (colorAnswer) {
-        alert("We have that!"); 
-        }
-    else {
-        alert("Sorry to hear that.")
+//     for(var i = 0; i < 2 ; i++ ){
+//         if (i === 0){
+//             boatQuestion = redBoat;
+//         } else if (i === 1) {
+//             boatQuestion = yellowBoat; 
+//         } else if (i === 2) {
+//             boatQuestion = blueBoat;
+//         }
+
+//         var colorAnswer = confirm("Want " + boatQuestion)
+//         if (colorAnswer) {
+//             alert("We have that!"); 
+//         } else {
+//             alert("Sorry to hear that.");
+//         }
+//         console.log("What is three strikes?"+ (threeStrikes))
+//         if (threeStrikes === 6){
+//             alert("We only have red, blue, and yellow boats. Check out the Truck or Plane links from my page."); 
+//         }
+//     }
+// }
+
+function redBlueYellow() {
+    var userColor = prompt("What color of boat would you like?");
+    userColor = userColor.toLowerCase();
+    console.log("The lower case test : ", userColor)
+    while (userColor !== "yellow" && userColor !== "red" && userColor !== "blue"){
+        userColor = prompt("Sorry, we only sell Red, Blue, or Yellow Boats. Which of those?");
+        userColor = userColor.toLowerCase();
     }
-}
-}
+};
+
+function salesRequest() {
+    var quantity = prompt("How many Boats would you like?");
+    var offer;
+    var price;
+    var cart='';
+
+    while (isNaN(quantity)){
+        quantity = prompt("Please enter how many you want as a numerical number.")
+    }
+    Number(quantity);
+
+    for(var i = 0; i < quantity ; i++ ){
+        price = Math.floor((Math.random()*567));
+        offer = 'There is a boat your could buy for '+ price +' dollars!';
+        cart = cart + offer;
+    } 
+
+    return cart;
 }
